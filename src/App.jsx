ead16f24fa3978b56cbfc18a6b3a45e299592de0
didacfrndz <fernandezbarcodidac@fpllefia.com>
2024-12-13 17:29:19 +0100

    primeraPrueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { Persona } from "./components/persona"
 import React from "react";
+import { Persona } from "./components/persona";
 export default function App(){
   console.log("App");
   return(
     <>
       <h1>Sesion 6 App</h1>
-      <Persona />
+      <div className="row">
+          <div className="col">
+            <Persona imgPersona="./public/melo.jpg" nombre="Lamelo Ball" /> 
+          </div>
+          <div className="col">
+            <Persona imgPersona="./public/jokic.jpg" nombre="Nikola Jokic"/>
+          </div>
+          <div className="col">
+            <Persona imgPersona="./public/leonard.jpg" nombre="Kawhi Leonard"/>
+          </div>
+      </div>
     </>
   )
 }
--- src/components/persona.jsx
+++ src/components/persona.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
 import React from "react"
-export function Persona(){
+export function Persona({imgPersona, nombre}){
     return(
         <>
             <div className="card" style={{width: "18rem"}}>
-                <img className="card-img-top" src="" alt="" />
+                <img className="card-img-top" src={imgPersona} alt="" />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
+                    <h5 className="card-title">{nombre}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
